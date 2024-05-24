3d022bd26465afc72c1958dee2b6b51d969baf40
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-05-24 19:12:43 +0300

    state kullanimi

---
+++ lib/anasayfa.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class Anasayfa extends StatefulWidget {
+  const Anasayfa({super.key});
+
+  @override
+  State<Anasayfa> createState() => _AnasayfaState();
+}
+
+class _AnasayfaState extends State<Anasayfa> {
+  int sayac = 0;
+  @override
+  Widget build(BuildContext context) {
+    print("Build calisti");
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Anasayfa"),
+        centerTitle: true,
+        backgroundColor: Colors.amber,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Sonuc: $sayac"),
+            ElevatedButton(onPressed: (){
+              setState(() {
+                sayac++;
+              });
+            }, child: const Text("Tıkla")),
+          ],
+        ),
+      ),
+    );
+  }
+}
