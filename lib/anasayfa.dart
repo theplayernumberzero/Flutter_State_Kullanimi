6571a40bd0ef9d8e0f60a8e21bc0e77db463e651
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-06-23 23:05:36 +0300

    widget içi kodlama teknikleri

--- lib/anasayfa.dart
+++ lib/anasayfa.dart
@@ -1,5 +1,6 @@
 import 'package:calisma_yapisi/kisiler.dart';
 import 'package:calisma_yapisi/oyun_ekrani.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Anasayfa extends StatefulWidget {
@@ -11,10 +12,16 @@ class Anasayfa extends StatefulWidget {
 
 class _AnasayfaState extends State<Anasayfa> {
   int sayac = 0;
+  bool kontrol = false;
   @override
   void initState() {
     super.initState();
     print("initState() calisti");
+  }
+
+  Future<int> toplama(int sayi1, int sayi2) async{
+    int toplam = sayi1 + sayi2;
+    return toplam;
   }
   @override
   Widget build(BuildContext context) {
@@ -42,6 +49,34 @@ class _AnasayfaState extends State<Anasayfa> {
                     print("Anasayfaya Dönüldü..");
               });
             }, child: const Text("Basla")),
+            Visibility(visible: kontrol, child: Text("Merhaba")),
+            Text("Merhaba", style: TextStyle(color: kontrol ? Colors.green : Colors.red),),
+            ((){
+              if(kontrol){
+                return const Text("Merhaba");
+              }else{
+                return const Text("Güle Güle");
+              }
+            }()),
+            ElevatedButton(onPressed: (){
+              setState(() {kontrol = true;});
+            }, child: const Text("Durum 1 (true)")),
+            ElevatedButton(onPressed: (){
+              setState(() {kontrol = false;});
+            }, child: const Text("Durum 1 (false)")),
+            FutureBuilder<int>(
+              future: toplama(10, 20),
+              builder: (context, snapshot){
+                if(snapshot.hasError){
+                  return Text("Error oluştu");
+                }
+                if(snapshot.hasData){
+                  return Text("Sonuc: ${snapshot.data}");
+                }else{
+                  return Text("Data yok");
+                }
+              },
+            ),
           ],
         ),
       ),
