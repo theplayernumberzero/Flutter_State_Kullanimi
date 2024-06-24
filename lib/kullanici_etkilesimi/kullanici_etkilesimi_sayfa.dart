55738f5df2052c5ef45f7e03a1774b54bf66faf5
theplayernumberzero <bxkuygulamalar.swe@gmail.com>
2024-06-24 22:57:37 +0300

    snackbar ve alert

--- lib/kullanici_etkilesimi/kullanici_etkilesimi_sayfa.dart
+++ lib/kullanici_etkilesimi/kullanici_etkilesimi_sayfa.dart
@@ -8,6 +8,8 @@ class KullaniciEtkilesimiSayfa extends StatefulWidget {
 }
 
 class _KullaniciEtkilesimiSayfaState extends State<KullaniciEtkilesimiSayfa> {
+  //TextField için
+  var tfControl = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,16 +22,75 @@ class _KullaniciEtkilesimiSayfaState extends State<KullaniciEtkilesimiSayfa> {
           child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               ElevatedButton(onPressed: (){
-
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                      content: const Text("Silmek ister misiniz ?"),
+                      action: SnackBarAction(label: "Evet", onPressed: (){
+                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: const Text("Silindi")));
+                      }
+                      ,),
+                )
+                );
               }, child: const Text("Snackbar")),
               ElevatedButton(onPressed: (){
-
+                ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: const Text("Silmek ister misiniz ?", style: TextStyle(color: Colors.indigoAccent),),
+                      backgroundColor: Colors.white30,
+                      action: SnackBarAction(label: "Evet", textColor: Colors.red, onPressed: (){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text("Silindi", style: TextStyle(color: Colors.red)),
+                                backgroundColor: Colors.white30,
+                            ));
+                      }
+                        ,),
+                    )
+                );
               }, child: const Text("Snackbar ÖZEL")),
               ElevatedButton(onPressed: (){
-
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context){
+                      return  AlertDialog(
+                        title: const Text("Baslık"),
+                        content: const Text("İçerik"),
+                        actions: [
+                          TextButton(onPressed: (){
+                            print("İptal Seçildi..");
+                            Navigator.pop(context); //geri tuşu işlemi. Ekranda boş bir yere dokunma ile aynı işlevi görür.
+                          }, child: const Text("İptal")),
+                          TextButton(onPressed: (){
+                            print("Tamam Seçildi..");
+                            Navigator.pop(context); //geri tuşu işlemi. Ekranda boş bir yere dokunma ile aynı işlevi görür.
+                          }, child: const Text("Tamam"))
+                        ],
+                      );
+                    }
+                );
               }, child: const Text("Alert")),
               ElevatedButton(onPressed: (){
-
+                showDialog(
+                    context: context,
+                    builder: (BuildContext context){
+                      return  AlertDialog(
+                        title: const Text("Kayıt işlemi"),
+                        content: TextField(controller: tfControl, decoration: const InputDecoration(hintText: "Veri Giriniz"),),
+                        backgroundColor: Colors.grey,
+                        actions: [
+                          TextButton(onPressed: (){
+                            print("İptal Seçildi..");
+                            Navigator.pop(context); //geri tuşu işlemi. Ekranda boş bir yere dokunma ile aynı işlevi görür.
+                          }, child: const Text("İptal", style: TextStyle(color: Colors.black),)),
+                          TextButton(onPressed: (){
+                            print("Alinan veri: ${tfControl.text}");
+                            Navigator.pop(context); //geri tuşu işlemi. Ekranda boş bir yere dokunma ile aynı işlevi görür.
+                            tfControl.text = ""; //okuma işleminden sonra textField içini temizleme
+                          }, child: const Text("Kaydet", style: TextStyle(color: Colors.black),))
+                        ],
+                      );
+                    }
+                );
               }, child: const Text("Alert ÖZEL")),
             ],
           )),
